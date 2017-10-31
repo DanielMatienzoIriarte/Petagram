c29876ee862beabe952a5d3d68b87a1973801c81
Daniel Matienzo Iriarte <daniel.matienzo.iriarte@gmail.com>
2017-10-31 01:46:14 -0400

    Add Toast and Snackbar

--- app/src/main/java/com/example/danmat/petagram/MainActivity.java
+++ app/src/main/java/com/example/danmat/petagram/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.danmat.petagram;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +22,16 @@ public class MainActivity extends AppCompatActivity {
         myStarIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                //Toast.makeText(getBaseContext(), getResources().getString(R.string.icon_text), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, getResources().getString(R.string.icon_text), Snackbar.LENGTH_SHORT)
+                        .setAction(getResources().getString(R.string.icon_text), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.i("SNACKBAR", "Click on snackbar");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimaryDark))
+                        .show();
             }
         });
     }
